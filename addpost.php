55a1cc7c6ea8e9eaaeb2bc9a6c66f1356375a109
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-08-17 11:58:28 +1200

    Initial commit

---
+++ addpost.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Add post.
+ *
+ * @package    local_dbapis
+ * @copyright  2023 Your Name <you@example.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/dbapis/addpost.php'));
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading(get_string('pluginname', 'local_dbapis'));
+
+require_login();
+
+if (isguestuser()) {
+    throw new moodle_exception('noguest');
+}
+
+$messageform = new \local_dbapis\form\message_form();
+
+if ($data = $messageform->get_data()) {
+
+    // We are getting the user input as is.
+    // TODO: Ensure user input is safe to use.
+    $message = required_param('message', PARAM_RAW);
+
+    // We are just displaying the form data here.
+    // TODO: Save the data to the database.
+    echo $OUTPUT->header();
+
+    echo html_writer::start_tag('div', ['class' => 'border p-3 my-3']);
+    echo $message;
+    echo html_writer::end_tag('div');
+
+    echo html_writer::link($PAGE->url, get_string('continue'), ['class' => 'btn btn-link']);
+
+    echo $OUTPUT->footer();
+
+    exit;
+}
+
+echo $OUTPUT->header();
+
+$messageform->display();
+
+echo $OUTPUT->footer();
---
+++ classes/form/message_form.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  2023 Your name <your@email>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_dbapis\form;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/formslib.php');
+
+class message_form extends \moodleform {
+    /**
+     * Define the form.
+     */
+    public function definition() {
+        $mform = $this->_form; // Don't forget the underscore!
+
+        $mform->addElement('textarea', 'message', get_string('yourmessage', 'local_dbapis')); // Add elements to your form.
+        $mform->setType('message', PARAM_TEXT); // Set type of element.
+
+        // If editing the form, load data from db.
+        if (isset($this->_customdata['message'])) {
+            $message = $this->_customdata['message'];
+
+            $mform->addElement('hidden', 'id', $message->id);
+            $mform->setType('id', PARAM_INT); // Set type of element.
+
+            $mform->setDefault('message', $message->message);
+        }
+
+        $submitlabel = get_string('submit');
+        $mform->addElement('submit', 'submitmessage', $submitlabel);
+    }
+}
